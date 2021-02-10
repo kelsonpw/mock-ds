480728d9727843e205b9313042c8aec3b9cfe86a
Kelson Warner <kelson.warner@amplitude.com>
2021-02-09 17:29:30 -0800

    docs: Add stories for Vertical/HorizontalBox

---
+++ src/components/Box/HorizontalBox.stories.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from "@storybook/react/types-6-0";
+
+import { VerticalBox, BoxProps, HorizontalBox } from "./Box";
+import { BoxAlignment, BoxJustification } from "./tokens";
+
+export default {
+  title: "Box/HorizontalBox",
+  component: VerticalBox,
+} as Meta;
+
+const Template: Story<BoxProps> = (args) => <HorizontalBox {...args} />;
+
+export const Vertical = Template.bind({});
+Vertical.args = {
+  children: "I am in a box!",
+  alignment: BoxAlignment.CENTER,
+  justification: BoxJustification.CENTER,
+  style: {
+    width: 400,
+    height: 400,
+  },
+};
